4073b0825dc64a40ceace27bfc6c3e1d556c6bf2
Dhruv Garg <garg.dh123@gmail.com>
2020-11-10 17:26:53 +0530

    test fix

--- src/test/java/doom/http/RequestParseQueryTest.java
+++ src/test/java/doom/http/RequestParseQueryTest.java
@@ -7,6 +7,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -14,9 +17,12 @@ public class RequestParseQueryTest {
     @Mock HttpExchange exchange;
 
     @Test
-    public void parsePathForQueries() {
+    public void parsePathForQueries() throws URISyntaxException {
+        URI uri = new URI("api/resource?id=231&name=john&lastname=doe");
+
         Mockito.when(exchange.getRequestMethod()).thenReturn("GET");
-        Mockito.when(exchange.getRequestURI().getPath()).thenReturn("api/resource?id=231&name=john&lastname=doe");
+        Mockito.when(exchange.getRequestURI()).thenReturn(uri);
+
         Request request = new Request(exchange);
 
         assertAll(
