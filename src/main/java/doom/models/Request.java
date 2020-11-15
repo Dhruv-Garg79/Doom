20928b6b93038b3c9a62dca0411d0d19418ec305
Dhruv Garg <garg.dh123@gmail.com>
2020-11-15 18:29:38 +0530

    test bug fix

--- src/main/java/doom/models/Request.java
+++ src/main/java/doom/models/Request.java
@@ -10,11 +10,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Request {
+    private final HttpExchange exchange;
+    private final Map<String, String> queryParams;
     private String path;
     private HttpMethods method;
-    private final HttpExchange exchange;
-
-    private final Map<String, String> queryParams;
     private Map<String, String> pathParams;
 
     public Request(HttpExchange exchange) {
@@ -25,24 +24,24 @@ public class Request {
 
         queryParams = parseQueryParams(exchange.getRequestURI().getQuery(), false);
 
-        Headers headers = exchange.getRequestHeaders();
-        String bodyContentType = headers.get("Content-Type").get(0);
-        System.out.println(bodyContentType);
-        System.out.println(this.hashCode() + "............................");
+        if (exchange.getRequestHeaders() != null) {
+            Headers headers = exchange.getRequestHeaders();
+            String bodyContentType = headers.get("Content-Type").get(0);
+            System.out.println(bodyContentType);
+            System.out.println(this.hashCode() + "............................");
 
-        try(InputStream bodyStream = exchange.getRequestBody()){
-            StringBuilder sb = new StringBuilder();
-            int count = 0;
-            byte[] buffer = new byte[4096];
-            while ((count = bodyStream.read(buffer)) > 0){
-                sb.append(new String(buffer));
+            try (InputStream bodyStream = exchange.getRequestBody()) {
+                StringBuilder sb = new StringBuilder();
+                int count = 0;
+                byte[] buffer = new byte[4096];
+                while ((count = bodyStream.read(buffer)) > 0) {
+                    sb.append(new String(buffer));
+                }
+                System.out.println(sb.toString());
+            } catch (IOException e) {
+                System.out.println(e.getLocalizedMessage());
             }
-            System.out.println(sb.toString());
         }
-        catch (IOException e){
-            System.out.println(e.getLocalizedMessage());
-        }
-
     }
 
     public String getPath() {
@@ -65,19 +64,18 @@ public class Request {
         return exchange;
     }
 
-    public String getQueryParam(String key){
+    public String getQueryParam(String key) {
         return queryParams.get(key);
     }
 
-    public String getPathParam(String key){
+    public String getPathParam(String key) {
         return pathParams.get(key);
     }
 
-    public Map<String, String> parseQueryParams(String path, boolean isRawPath){
+    public Map<String, String> parseQueryParams(String path, boolean isRawPath) {
         Map<String, String> map = new HashMap<>();
 
-        if (path == null || path.isBlank())
-            return map;
+        if (path == null || path.isBlank()) return map;
 
         int n = path.length();
         int i = 0;
@@ -92,14 +90,14 @@ public class Request {
         StringBuilder key = new StringBuilder();
         StringBuilder val = new StringBuilder();
 
-        while (i < n){
+        while (i < n) {
             while (i < n && path.charAt(i) != '=') {
                 key.append(path.charAt(i));
                 i++;
             }
             i++;
 
-            while (i < n && path.charAt(i) != '&'){
+            while (i < n && path.charAt(i) != '&') {
                 val.append(path.charAt(i));
                 i++;
             }
--- src/test/java/doom/http/RequestParseQueryTest.java
+++ src/test/java/doom/http/RequestParseQueryTest.java
@@ -1,12 +1,17 @@
 package doom.http;
 
 import com.sun.net.httpserver.HttpExchange;
+import doom.models.Request;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.net.URI;
 import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class RequestParseQueryTest {
@@ -14,18 +19,17 @@ public class RequestParseQueryTest {
 
     @Test
     public void parsePathForQueries() throws URISyntaxException {
-        // TODO
-//        URI uri = new URI("api/resource?id=231&name=john&lastname=doe");
-//
-//        Mockito.when(exchange.getRequestMethod()).thenReturn("GET");
-//        Mockito.when(exchange.getRequestURI()).thenReturn(uri);
+        URI uri = new URI("api/resource?id=231&name=john&lastname=doe");
 
-//        Request request = new Request(exchange);
-//
-//        assertAll(
-//                () -> assertEquals(request.getQueryParam("id"), "231"),
-//                () -> assertEquals(request.getQueryParam("name"), "john"),
-//                () -> assertEquals(request.getQueryParam("lastname"), "doe"),
-//                () -> assertNull(request.getQueryParam("email")));
+        Mockito.when(exchange.getRequestMethod()).thenReturn("GET");
+        Mockito.when(exchange.getRequestURI()).thenReturn(uri);
+
+        Request request = new Request(exchange);
+
+        assertAll(
+                () -> assertEquals(request.getQueryParam("id"), "231"),
+                () -> assertEquals(request.getQueryParam("name"), "john"),
+                () -> assertEquals(request.getQueryParam("lastname"), "doe"),
+                () -> assertNull(request.getQueryParam("email")));
     }
 }
